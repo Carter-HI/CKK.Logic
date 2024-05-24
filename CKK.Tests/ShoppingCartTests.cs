e2cf7fbd4911b255f44f644bfcfacfbc9349736a
CARTER WESTERGARD <CARTER.WESTERGARD0028@OMAIL.OTECH.EDU>
2024-05-24 15:01:06 -0600

    finish removingproducttest and worked on gettotaltest

--- CKK.Tests/ShoppingCartTests.cs
+++ CKK.Tests/ShoppingCartTests.cs
@@ -36,13 +36,14 @@ namespace CKK.Tests
                 //Assemble
                 ShoppingCart myshoppingCart = new ShoppingCart(new Customer { });
                 Product product1 = new Product();
-                int quantity = 0; 
+                int quantity = 1;
                 product1.SetId(0);
+                myshoppingCart.AddProduct(product1);
                 myshoppingCart.RemoveProduct(product1, quantity);
                 ShoppingCartItem expectingItem = myshoppingCart.GetProduct(1);
                 int expected = 0;
                 //Act
-                int actual = expectingItem.GetProduct().GetId();
+                int actual = expectingItem.GetQuantity();
                 //Assert
                 Assert.Equal(expected, actual);
 
@@ -60,18 +61,19 @@ namespace CKK.Tests
                 //Assemble
                 ShoppingCart myshoppingCart = new ShoppingCart(new Customer { });
                 Product product1 = new Product();
-                product1.SetId(123);
+                product1.SetId(0);
+                product1.SetPrice(123);
                 myshoppingCart.AddProduct(product1);
                 ShoppingCartItem expectingItem = myshoppingCart.GetProduct(1);
-                int expected = 123;
+                decimal expected = 123;
                 //Act
-                int actual = expectingItem.GetProduct().GetId();
+                decimal actual = expectingItem.GetProduct().GetPrice();
                 //Assert
                 Assert.Equal(expected, actual);
             }
             catch
             {
-                throw new XunitException("The item's id was not added. ");
+                throw new XunitException("The price is incorrect.");
             }
         }
     }
