9e2bb3cba54d43a000824a97ea99bf514044c744
CARTER WESTERGARD <CARTER.WESTERGARD0028@OMAIL.OTECH.EDU>
2024-12-12 14:56:53 -0700

    added exception classes

---
+++ CKK.Logic/Exceptions/InvalidIdException.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CKK.Logic.Exceptions
+{
+    public class InvalidIdException : Exception
+    {
+        public InvalidIdException() : base("No numbers 0 or lower.")
+        {
+
+        }
+        public InvalidIdException(string messageValue) : base(messageValue)
+        {
+
+        }
+        public InvalidIdException(string messageValue, Exception inner) : base(messageValue, inner)
+        {
+
+        }
+    }
+}
